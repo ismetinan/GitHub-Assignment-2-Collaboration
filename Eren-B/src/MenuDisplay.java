c32dad594ba282ba5da6865eef30a6cbbbc34f0a
Eren Kerimoğlu <erenswim@gmail.com>
2024-09-25 16:39:24 +0300

    Kooolaaay gelsinnn

--- Eren-B/src/MenuDisplay.java
+++ Eren-B/src/MenuDisplay.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
+
+
+
 
 public class MenuDisplay {
 
     public static void main(String[] args) {
         Scanner myscan = new Scanner(System.in);
         String choice;
+        System.out.print("Enter requested size of array: ");
+        int size = myscan.nextInt();
         System.out.println("1-Sum of odd numbered indexes in given array");
         System.out.println("2-Sum of even numbered indexes in given array");
         System.out.println("3-Finds the avarage of the array");
@@ -13,29 +20,39 @@ public class MenuDisplay {
         System.out.println("6-Find the maximum value in the array");
         System.out.println("7-Exit");
         System.out.print("Enter your choice: ");
-        choice=myscan.nextLine();
-        int arr[]=new int[8];
-
+        choice=myscan.next();
+        System.out.println();
+        int[] arr =new int[size];
+        arr =  arraytask1.createRandomArray(size);
         while(!choice.equals("Exit")&& !choice.equals("7")){
             if(choice.equals("1")){
-                SumOddEven.sumOfOddIndexes(arr);
+                int sum = SumOddEven.sumOfOddIndexes(arr);
+                System.out.println("Sum of odd indexed numbers: " + sum );
             }
             else if(choice.equals("2")){
-                SumOddEven.sumOfEvenIndexes(arr);
+                int sum1 = SumOddEven.sumOfEvenIndexes(arr);
+                System.out.println("Sum of even indexed integers: " + sum1);
             }
             else if (choice.equals("3")){
-                Avarage.findAvarage(arr);
+               int ava= Avarage.findAvarage(arr);
+                System.out.println("Avarage of your array is : " + ava );
             }
             else if(choice.equals("4")){
                 int av=Avarage.findAvarage(arr);
-                Avarage.differFromAvarage(av, arr);
+                int[] diff = new int[size];
+                diff=Avarage.differFromAvarage(av, arr);
+                System.out.println("Each elements differ from the avarage: " + Arrays.toString(diff) );
             }
             else if(choice.equals("5")){
-                MinMaxFinder.MinFinder(arr);
+                int min=MinMaxFinder.MinFinder(arr);
+                System.out.println("Min element of your array is: " + min );
             }
             else if(choice.equals("6")){
-                MinMaxFinder.MaxFinder(arr);
+                int max=MinMaxFinder.MaxFinder(arr);
+                System.out.println("Max element of your array is: " + max);
+            
             }
+            System.out.println();
              
             System.out.println("1-Sum of odd numbered indexes in given array");
             System.out.println("2-Sum of even numbered indexes in given array");
@@ -45,13 +62,14 @@ public class MenuDisplay {
             System.out.println("6-Find the maximum value in the array");
             System.out.println("7-Exit");
             System.out.print("Enter your choice: ");
-            choice=myscan.nextLine();
+            choice=myscan.next();
             System.out.println();
 
         }
         if(choice.equals("7")){
             System.out.println("Kooooolaay gelsiinn");
         }
+        myscan.close();
         
     }
 }
--- Eren-B/src/arraytask1.java
+++ Eren-B/src/arraytask1.java
@@ -1,16 +1,12 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+
+
 import java.util.Random;
-import java.util.Scanner;
+
 
 
 public class arraytask1 {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int size = scan.nextInt();
-        System.out.println(Arrays.toString(createRandomArray(size)));
-    }
+    
     public static int[] createRandomArray (int num)
     {
         Random random = new Random();
